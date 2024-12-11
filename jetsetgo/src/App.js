b603112ab2f303f5b2d3a2c281db193494763f7c
Amandeep Kaur Singh <107598323+amandeepksingh@users.noreply.github.com>
2024-12-10 23:59:15 -0500

    Update App.js

--- jetsetgo/src/App.js
+++ jetsetgo/src/App.js
@@ -1,3 +1,61 @@
+// import React from "react";
+// import { BrowserRouter as Router, Routes, Route, Outlet, useLocation } from "react-router-dom";
+// import SignIn from "./components/signin/Signin";
+// import Profile from "./pages/Profile";
+// import FavoriteDestinations from "./pages/FavoriteDestinations";
+// import Header from "./components/header/Header";
+// import Footer from "./components/footer/Footer";
+// import Home from "./pages/Home";
+// import Itineraries from "./pages/Itineraries";
+// import CreateProfile from "./pages/CreateProfile";
+// import GroupChatsPage from "./pages/ViewChats";
+// import GroupChat from "./pages/GroupChat";
+// import Location from "./pages/Location";
+// import EditProfile from "./pages/EditProfile";
+// import OtherUserProfile from "./pages/OtherUserProfile";
+
+// const Layout = () => {
+//   const location = useLocation();
+
+//   // List of routes where the header should NOT be shown
+//   const noHeaderRoutes = ["/create-profile"];
+
+//   return (
+//     <div className="flex flex-col min-h-screen">
+//       {/* Render Header only if the current path is NOT in noHeaderRoutes */}
+//       {!noHeaderRoutes.includes(location.pathname) && <Header />}
+//       <main className="flex-grow">
+//         <Outlet />
+//       </main>
+//       <Footer />
+//     </div>
+//   );
+// };
+
+// function App() {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<SignIn />} />
+//         <Route element={<Layout />}>
+//           <Route path="/home" element={<Home />} />
+//           <Route path="/profile" element={<Profile />} />
+//           <Route path="/favorite-destinations" element={<FavoriteDestinations />} />
+//           <Route path="/itineraries" element={<Itineraries />} />
+//           <Route path="/create-profile" element={<CreateProfile />} />
+//           <Route path="/group-chats" element={<GroupChatsPage />} />
+//           <Route path="/chat" element={<GroupChat />} />
+//           <Route path="/location" element={<Location />} />
+//           <Route path="/edit-profile" element={<EditProfile />} />
+//           <Route path="/other-user-profile" element={<OtherUserProfile />} />
+//         </Route>
+//       </Routes>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Outlet, useLocation } from "react-router-dom";
 import SignIn from "./components/signin/Signin";
@@ -12,7 +70,6 @@ import GroupChatsPage from "./pages/ViewChats";
 import GroupChat from "./pages/GroupChat";
 import Location from "./pages/Location";
 import EditProfile from "./pages/EditProfile";
-import OtherUserProfile from "./pages/OtherUserProfile";
 
 const Layout = () => {
   const location = useLocation();
@@ -47,7 +104,7 @@ function App() {
           <Route path="/chat" element={<GroupChat />} />
           <Route path="/location" element={<Location />} />
           <Route path="/edit-profile" element={<EditProfile />} />
-          <Route path="/other-user-profile" element={<OtherUserProfile />} />
+          <Route path="/location/:placeId" element={<Location />} />
         </Route>
       </Routes>
     </Router>
@@ -55,3 +112,4 @@ function App() {
 }
 
 export default App;
+
